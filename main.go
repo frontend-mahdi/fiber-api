9a4cabfe87f60f8a894514ba2c044ccbed87e78b
frontend-mahdi <mahdi.313.ab@gmail.com>
2025-06-04 11:30:25 +0330

    fix: Added cors middleware

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
 
 func main() {
-    app := fiber.New()
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, Fiber! 🚀")
-    })
-    app.Listen(":3000")
+	app := fiber.New()
+
+	// Add CORS middleware
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:5173, https://your-deployed-frontend.com", // change as needed
+		AllowHeaders:     "Origin, Content-Type, Accept",
+		AllowCredentials: true,
+	}))
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, Fiber! 🚀")
+	})
+	app.Listen(":3000")
 }
